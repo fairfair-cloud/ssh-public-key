cdb1c44ad053e1e3126c941a5f7eb25eac51d5ba
romainlavabre <romainlavabre98@gmail.com>
2025-06-18 21:37:29 +0200

    INITIAL

--- update-conf.sh
+++ update-conf.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "[INFO] Pulling last public keys..."
 git pull
 
+echo "[INFO] Removing old public keys..."
 rm /home/ubuntu/.ssh/authorized_keys
 
+echo "[INFO] Installing new public keys..."
 cp authorized_keys /home/ubuntu/.ssh/authorized_keys
 
-ssh-agent -k
+echo "[INFO] Public key installed !"
