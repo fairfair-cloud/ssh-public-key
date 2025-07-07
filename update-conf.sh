862f3da21e60de685d91f0c9289cb653190b4bec
romainlavabre <romainlavabre98@gmail.com>
2025-07-07 12:08:32 +0200

    INITIAL

--- update-conf.sh
+++ update-conf.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "[ERROR] Provide environement (dev, staging, live)"
-    exit 1
-fi
-
 echo "[INFO] Pulling last public keys..."
 git pull
 
@@ -12,6 +7,6 @@ echo "[INFO] Removing old public keys..."
 rm /home/ubuntu/.ssh/authorized_keys
 
 echo "[INFO] Installing new public keys..."
-cp authorized_keys_$1 /home/ubuntu/.ssh/authorized_keys
+cp authorized_keys /home/ubuntu/.ssh/authorized_keys
 
 echo "[INFO] Public key installed !"
